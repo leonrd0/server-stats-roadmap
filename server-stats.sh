4abf9c98118cf2897eecb0082c3b94bd419b8dbc
lcarvalho5 <lcarvalho5@lenovo.com>
2024-11-07 15:39:20 -0300

    add requirements

---
+++ server-stats.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# ---------------------------------------
+# Requirements
+#   You are required to write a script server-stats.sh that can analyse basic server performance stats. You should be able to run the script on any Linux server and it should give you the following stats:
+    #    Total CPU usage
+    #    Total memory usage (Free vs Used including percentage)
+    #    Total disk usage (Free vs Used including percentage)
+    #    Top 5 processes by CPU usage
+    #    Top 5 processes by memory usage
+# Stretch goal: Feel free to optionally add more stats such as os version, uptime, load average, logged in users, failed login attempts etc.
+# ---------------------------------------
+
+
+# ---------------------------------------
+# functions:
+# to display cpu usage
+
+# to display ram usage
+
+# to display disk usage
+
+# to display top 5 processes by CPU usage
+
+# to display top 5 processes by memory usage
+
+# ---------------------------------------
+
+# ---------------------------------------
+# main code
+
+
+
+# ---------------------------------------
